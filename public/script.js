df8cb454e3abb954fafd7d8fdb1ebe20c856d493
Kamil Majewski <majkel1205@gmail.com>
2021-04-23 23:23:42 +0200

    POSTowanie wszystkiego poza obrazkiem

--- public/script.js
+++ public/script.js
@@ -1,14 +1,22 @@
 function sendMail() {
-        addres = document.getElementById("inputSpamEmail").value,
-        mail = document.getElementById("inputEmailText").value
+        const addres = document.getElementById("inputSpamEmail").value
+        const mail = document.getElementById("inputEmailText").value
+        const sendDate = document.getElementById("sendDate").value
+        const sendTime = document.getElementById("sendTime").value
+        const data = {addres, mail, sendDate, sendTime}
+        
 
     fetch(`={"addres":${addres}, "mail":${mail}`, {
-        method: 'POST'
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+          },
+        body: JSON.stringify(data)
     })
     .then(res => {
         return res.text()
     })
     .catch((error)=> {
-        alert(error)
+        alert(`Error: ${error}`)
     })
 }
